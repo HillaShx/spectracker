71a3acd2ad23fea9e70f92f924fd734832080095
HillaShx <hilla.sh@gmail.com>
2022-10-10 19:19:55 +0300

    add integration test for create_user route

--- database.py
+++ database.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# load_dotenv('/home/hillash/dev/spectracker-api/local.env')  # for local run
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ.get('PGUSER')}:{os.environ.get('PGPASSWORD')}@{os.environ.get('PGHOST')}:{os.environ.get('PGPORT')}/{os.environ.get('PGDB')}"
